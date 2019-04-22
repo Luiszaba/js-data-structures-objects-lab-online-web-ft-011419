9cc26355a47ca17a2da29dceaa78a5a1c9cd3669
Luis Zabala <luis.zabala076@gmail.com>
2019-04-22 00:58:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 
 const driver = {};
 
+// update driver with a key and a value 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, key, value);
+  return Object.assign({}, driver, { [key]: value });
+}
+
+// destro update with key and value
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
+
+// delete driver by key
+function deleteFromDriverByKey(driver, key) {
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+// destro delete driver from key
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+
+  return driver;
 }
